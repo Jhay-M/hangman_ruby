008cb5053cd991eec6722069f6ce59012aab66ec
Joshua Malomo <rexunborn@gmail.com>
2020-10-14 18:34:38 +0100

    Done with Game Logic

--- hangman.rb
+++ hangman.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Game
   def initialize(name)
     @name = name
@@ -9,10 +7,17 @@ class Game
   end
 
   def play
-    @letter = gets.chomp
-    turn_count
-    display_blanks
+    @letter = gets.chomp.downcase
+    create_blanks if @blanks.nil?
     guess_check
+    turn_count
+    check_game_over
+  end
+
+  def choose_word
+    @dictionary = File.readlines '5desk.txt'
+    @dictionary.map!(&:chomp)
+    choose_sample
   end
 
   def choose_sample
@@ -20,30 +25,43 @@ class Game
     @secret_word = @secret_word_options.sample.downcase
   end
 
-  def turn_count
-    @turns -= 1
-    puts "Turns Left: #{@turns}"
-  end
-
-  def display_blanks
-    if @blanks.nil?
-      @blanks = @secret_word.split('').map do |char|
-        char = '_'
-      end
+  def create_blanks
+    @blanks = @secret_word.split('').map do |char|
+      char = '_'
     end
-    p @blanks
   end
 
   def guess_check
-    @letter.downcase!
-    @secret_word.split('').each_with_index do |value, idx|
-      @blanks[idx] = @letter if value == @letter
+    if !@secret_word.include?(@letter)
+      @turns -= 1
+      tries = (@wrong_guess << @letter).join(', ')
+      puts "Wrong Guesses: (#{tries})"
+    else
+      @secret_word.split('').each_with_index do |value, idx|
+        @blanks[idx] = @letter if value == @letter
+      end
+    end
+    print @blanks
+  end
+
+  def turn_count
+    puts "Turns Left: #{@turns}"
+  end
+
+  def check_game_over
+    if @turns == -1
+      puts "Game Over! Word was #{@secret_word}."
+      reset
+    elsif @blanks.join('') == @secret_word
+      puts "Congratulations #{@name}. You guessed #{@secret_word}!"
+      reset
     end
   end
 
-  def choose_word
-    @dictionary = File.readlines '5desk.txt'
-    @dictionary.map!(&:chomp)
+  def reset
     choose_sample
+    @turns = 10
+    @wrong_guess = []
+    @blanks = nil
   end
 end
